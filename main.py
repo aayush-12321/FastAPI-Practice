a3e6e38ff7b9bca1b274d66246e24e4aa238d90a
aayush-12321 <aayushparajuli23@gmail.com>
2026-08-11 23:44:29 +0545

    Upto SQL Database

--- main.py
+++ main.py
@@ -47,6 +47,51 @@ async def validation_exception_handler(request, exc: RequestValidationError):
 #! ##################################################
 
 
+
+#! Day 7 ############################################
+
+# Middleware
+
+import time
+
+@app.middleware("http")
+async def add_process_time_header(request: Request, call_next):
+    start_time = time.perf_counter()
+    response = await call_next(request)
+    process_time = time.perf_counter() - start_time
+    response.headers["X-Process-Time"] = str(process_time)
+    return response
+
+
+# CORS (Cross-Origin Resource Sharing)
+
+from fastapi.middleware.cors import CORSMiddleware
+
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+#! SQl Databases
+
+from routers.day7 import create_db_and_tables
+
+@app.on_event("startup")
+def on_startup():
+    create_db_and_tables()
+
+#! ##################################################
+
+
 @app.get("/")
 def home():
     return {"message": "Welcome to the FastAPI application!"}
